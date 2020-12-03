c60ca125f13e802dd9d4b1db28877cd093c391a3
Omar Baranek <obaranek@uwaterloo.ca>
2020-12-03 18:21:12 -0500

    added PARK as a ResourceType

--- ResourceType.h
+++ ResourceType.h
@@ -1,13 +1,6 @@
 #ifndef __RESOURCETYPE_H_
 #define __RESOURCETYPE_H_
 
-enum ResourceType {
-BRICK,
-ENERGY,
-GLASS,
-HEAT,
-WIFI
-};
-
+enum ResourceType { BRICK, ENERGY, GLASS, HEAT, WIFI, PARK };
 
 #endif // __RESOURCETYPE_H_
